83032a641b66574cb3f878de0cf6d9c142cfa5a3
bhorkar <abhijeet.bhorkar@gmail.com>
2017-07-30 22:37:28 -0700

    add corrections

--- car_capture_v1.py
+++ car_capture_v1.py
@@ -20,6 +20,7 @@ import numpy as np
 import scipy.io as sio
 import caffe, os, sys, cv2
 import argparse
+from numpy import linalg as LA 
 #!/usr/bin/env python
 from flask import Flask, render_template, Response
 import cv2
@@ -57,6 +58,27 @@ NETS = {'vgg16': ('VGG16',
                   'ZF_faster_rcnn_final.caffemodel')}
 
 
+def describe(net, imagePath):
+        mu = np.load('./py-faster-rcnn/caffe-fast-rcnn/' + 'python/caffe/imagenet/ilsvrc_2012_mean.npy')
+         # average over pixels to obtain the mean (BGR) pixel values
+        mu = mu.mean(1).mean(1)
+
+        transformer = caffe.io.Transformer(
+                {'data': net.blobs['data'].data.shape})
+        transformer.set_transpose('data', (2, 0, 1))
+        transformer.set_mean('data', mu)
+        transformer.set_raw_scale('data', 255)
+        transformer.set_channel_swap('data', (2, 1, 0))
+        InterMediateLayer = 'fc7'
+
+        image = caffe.io.load_image(imagePath)
+        transformed_image = transformer.preprocess('data', image)
+        net.blobs['data'].data[...] = transformed_image
+        output = net.forward()
+        feat = net.blobs[InterMediateLayer].data[0]
+        shape = feat.shape
+        return feat
+
 
 
 def demo(net, im):
@@ -75,6 +97,7 @@ def demo(net, im):
     # Visualize detections for each class
     CONF_THRESH = 0.8
     NMS_THRESH = 0.3
+    add = False; 
     for cls_ind, cls in enumerate(CLASSES[1:]):
         cls_ind += 1 # because we skipped background
         cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
@@ -85,18 +108,16 @@ def demo(net, im):
         dets = dets[keep, :]
         inds = np.where(dets[:, -1] >= 0.5)[0]
 
-        add = False; 
         for i in inds:
             add = True
             bbox = dets[i, :4]
             score = dets[i, -1]
-            print score, cls
-            if cls == "car" or cls == "person":
+            if ("car" in cls) or ("person" in cls):
                 pt1 =  (bbox[0], bbox[1])
                 pt2 = (bbox[2], bbox[3])
-            
                 cv2.rectangle(im,pt1, pt2, (0,0,255), 2)
                 cv2.putText(im,'{:s} {:.3f}'.format(cls, score),(bbox[0], (int)((bbox[1]- 2))), cv2.FONT_HERSHEY_SIMPLEX , 0.8, (0,255,0), 1)
+
     return im, add;
 
         
@@ -128,7 +149,7 @@ if __name__ == '__main__':
                             'faster_rcnn_alt_opt', 'faster_rcnn_test.pt')
     caffemodel = os.path.join(cfg.DATA_DIR, 'faster_rcnn_models',
                               NETS['vgg16'][1])
-
+     
     if not os.path.isfile(caffemodel):
         raise IOError(('{:s} not found.\nDid you run ./data/script/'
                        'fetch_faster_rcnn_models.sh?').format(caffemodel))
@@ -148,6 +169,8 @@ if __name__ == '__main__':
     for i in xrange(2):
         _, _= im_detect(net, im)
     index = 0
+    lru_cache_new = [];
+    lru_cache_desc_new = [];
     while True:
         if 1 :
             success, img = vid.read()
@@ -158,7 +181,32 @@ if __name__ == '__main__':
             minY = int(h*0.2)
             maxY = int(w*0.8)
             check_car = False;
+            lru_cache_old = lru_cache_new;
+            lru_cache_new = [];
+            lru_cache_desc_old = lru_cache_desc_new;
+            lru_cache_desc_new = [];
             for (x,y,w,h) in cars:
+                bool_break = 0;
+                for x1,y1,w1,h1 in lru_cache_old:
+                    if abs(x1-x) < 20  and abs (y1-y) < 20 and abs(w1-w) < 20 and abs(h1-h) < 20:
+                    #same car as previus frame
+                        bool_break = 1;
+                        break;
+                if bool_break == 1:
+                    continue;
+                #print im.shape
+                pic = img[int(y):int(y+h), int(x):int(x+w),:]
+                cv2.imwrite('t1.jpg', pic)
+                desc = describe(net,'t1.jpg');
+                for desc1 in lru_cache_desc_old:
+                    error = LA.norm(desc1 - np.array(desc))/(LA.norm(desc));
+                    print "error", error
+                    if error < 0.01:
+                        bool_break = 1;
+                if bool_break == 1:
+                    continue;
+                lru_cache_new.append([x,y,w,h])
+                lru_cache_desc_new.append(np.array(desc))
                 if y > minY and y < maxY and w > 20:
                     print x,y,w,h
                     cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
@@ -172,7 +220,11 @@ if __name__ == '__main__':
                  filename = dictname +  '/count_' + str(index) + '.jpg';
                  print "saving" + filename;
                  cv2.imwrite(filename, img);
-                 time.sleep(1) 
+                 timer = Timer()
+                 timer.tic()
+                 time.sleep(6) 
+                 timer.toc()
+                 print timer.total_time
                  index = index + 1;
             #cv2.imshow('IPWebcam',img)
 	    cv2.imwrite('t.jpg', img)
